ce5e8dd860c2fcc427205d1bb4f7e64bbf801eb2
benjamin.johnson <benjamin.johnson@madetech.com>
2024-01-23 14:04:37 +0000

    tripleDigitSquares function

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // TODO: Write a function that squares every number in an array and returns any triple digit results. Return an empty array if there are no triple digit results.
 // Example: [3, 4, 1, 7, 10, 15] -> [100, 225]
+
+function tripleDigitSquares(numbers) {
+  squared = numbers.map(x => x**2);
+  tripleDigits = squared.filter(x => 99 < x < 1000);
+  return tripleDigits;
+}
